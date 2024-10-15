1b3fa941da47e5deecafb918a77c0cd9d5d43311
Timur Olur <timurolur@gmail.com>
2024-10-15 09:44:11 +0300

    Updates algorithms repo usage - HashMap with city64 hash is still biggest overhead.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use algorithms::{buffer_utils::FastBufferUtils, hash::cityhash::hash_fn::cityhash_64_with_seed};
+use algorithms_buffer_utils::FastBufferUtils;
+use algorithms_hash::cityhash::hash_fn::cityhash_64_with_seed;
 use core::slice;
 use libc::{c_void, mmap64, munmap, MAP_FAILED, MAP_PRIVATE, PROT_READ};
 use std::{
@@ -11,7 +12,7 @@ use std::{
     ptr::null_mut,
     str::from_utf8_unchecked,
     sync::Arc,
-    thread::{available_parallelism, ScopedJoinHandle},
+    thread::{self, available_parallelism, JoinHandle},
 };
 
 struct City64Hasher {
@@ -85,12 +86,26 @@ impl StationData {
 }
 
 /// Good'ol mmap, see [man(mmap)](https://www.man7.org/linux/man-pages/man2/mmap.2.html)
-#[derive(Debug, Clone)]
-pub struct Mmap {
-    inner: Arc<MmapInner>,
+#[derive(Debug)]
+struct Mmap {
+    addr: *const c_void,
+    len: usize,
+}
+
+unsafe impl Send for Mmap {}
+unsafe impl Sync for Mmap {}
+
+impl Drop for Mmap {
+    fn drop(&mut self) {
+        let res = unsafe { munmap(self.addr.cast_mut(), self.len) };
+        if res != 0 {
+            eprintln!("Unable to drop mmapped region, code : {}", res);
+        }
+    }
 }
 
 impl Mmap {
+    #[inline]
     pub fn new(fd: File) -> Result<Self, BrcErrors> {
         const FLAGS: i32 = MAP_PRIVATE;
 
@@ -111,92 +126,105 @@ impl Mmap {
             return Err(BrcErrors::MmapFailed);
         }
 
-        Ok(Self {
-            inner: Arc::from(MmapInner { addr, len }),
-        })
+        Ok(Self { len, addr })
     }
 
     #[inline]
-    pub fn as_byte_slice(&self) -> &[u8] {
-        unsafe { slice::from_raw_parts(self.inner.addr.cast::<u8>(), self.inner.len) }
+    pub fn as_byte_slice<'a>(&'a self) -> &'a [u8] {
+        unsafe { slice::from_raw_parts::<'a, u8>(self.addr.cast::<u8>(), self.len) }
     }
 
     #[inline]
     pub fn len(&self) -> usize {
-        self.inner.len
+        self.len
     }
 }
 
-#[derive(Debug)]
-struct MmapInner {
-    addr: *const c_void,
+#[derive(Clone)]
+struct MmapSlice {
+    inner: Arc<Mmap>,
+    offset: usize,
     len: usize,
 }
 
-unsafe impl Send for MmapInner {}
-unsafe impl Sync for MmapInner {}
-
-impl Drop for MmapInner {
-    fn drop(&mut self) {
-        let res = unsafe { munmap(self.addr.cast_mut(), self.len) };
-        if res != 0 {
-            eprintln!("Unable to drop mmapped region, code : {}", res);
+impl MmapSlice {
+    #[inline]
+    pub fn as_byte_slice<'a>(&'a self) -> &'a [u8] {
+        unsafe {
+            slice::from_raw_parts::<'a, u8>(
+                self.inner.addr.cast::<u8>().byte_add(self.offset),
+                self.len,
+            )
         }
     }
 }
 
 /// Splits a slice into multiple chunks with considiration of LF positions.
 /// It does not gurantees equal chunk sizes since it looks for nearest LF from chunk offset.
-pub struct ChunkedLinesIter<'a> {
-    bytes: &'a [u8],
+pub struct ChunkedLinesIter<T> {
+    inner: T,
     pos: usize,
     chunk_size: usize,
 }
 
-impl<'a> ChunkedLinesIter<'a> {
+impl<T> ChunkedLinesIter<T> {
+    #[inline]
     pub fn chunk_by(mut self, size: usize) -> Self {
         self.chunk_size = size;
         self
     }
 }
 
-impl<'a> From<&'a [u8]> for ChunkedLinesIter<'a> {
-    fn from(value: &'a [u8]) -> Self {
+impl From<Arc<Mmap>> for ChunkedLinesIter<Arc<Mmap>> {
+    fn from(value: Arc<Mmap>) -> Self {
         Self {
             pos: 0,
-            bytes: value,
+            inner: value,
             chunk_size: 65536, // Default is 64KiB
         }
     }
 }
 
-impl<'a> Iterator for ChunkedLinesIter<'a> {
-    type Item = &'a [u8];
+impl Iterator for ChunkedLinesIter<Arc<Mmap>> {
+    type Item = MmapSlice;
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
         let start = self.pos;
         let end = start + self.chunk_size - 1;
+        let len = self.inner.len;
 
-        if end >= self.bytes.len() {
-            if self.pos < self.bytes.len() {
-                self.pos = self.bytes.len();
-                Some(&self.bytes[start..])
+        if end >= len {
+            if self.pos < len {
+                self.pos = len;
+
+                Some(Self::Item {
+                    len: len - start,
+                    inner: self.inner.clone(),
+                    offset: start,
+                })
             } else {
                 None
             }
         } else {
             // End position could be middle of the row. We simply look ahead to find nearest LF (0x0A) before slicing.
-            let look_ahead_chunk = &self.bytes[end..];
+            let look_ahead_chunk = &self.inner.as_byte_slice()[end..];
 
             if let Some(lf_idx) = look_ahead_chunk.fast_find(b'\n') {
                 let chunk_end = end + lf_idx;
 
                 self.pos = chunk_end + 1;
-                Some(&self.bytes[start..=chunk_end])
+                Some(Self::Item {
+                    len: chunk_end - start + 1,
+                    inner: self.inner.clone(),
+                    offset: start,
+                })
             } else {
-                self.pos = self.bytes.len();
-                Some(&self.bytes[start..])
+                Some(Self::Item {
+                    len: len - start,
+                    inner: self.inner.clone(),
+                    offset: start,
+                })
             }
         }
     }
@@ -205,11 +233,13 @@ impl<'a> Iterator for ChunkedLinesIter<'a> {
 type StationHashMap = HashMap<HashKey, StationData, City64HasherBuilder>;
 type HashKey = u128;
 
-/// Converts UTF-8 number character to binary number value without any check.
 macro_rules! parse_number {
-    ($v:expr) => {
-        $v - 48
-    };
+    ($v:expr) => {{
+        let mut val: u64 = $v;
+        val = (val & 0x0F0F0F0F0F0F0F0F) * 2561 >> 8;
+        val = (val & 0x00FF00FF00FF00FF) * 6553601 >> 16;
+        (val & 0x0000FFFF0000FFFF) * 42949672960001 >> 32
+    }};
 }
 
 // Parses float values between -99.9 to 99.9
@@ -217,30 +247,20 @@ macro_rules! parse_number {
 fn parse_f64(value: &[u8]) -> f64 {
     match value {
         [b'-', h1, h0, b'.', l] => {
-            let nh1 = parse_number!(h1) * 10;
-            let nh0 = parse_number!(h0);
-            let nl = parse_number!(l) as f64 * 0.1;
-
-            -((nh1 + nh0) as f64 + nl)
+            let val = parse_number!(u64::from_le_bytes([0, 0, 0, 0, 0, *h1, *h0, *l]));
+            (val as f64) * -0.1
         }
         [b'-', h0, b'.', l] => {
-            let nh0 = parse_number!(h0);
-            let nl = parse_number!(l) as f64 * 0.1;
-
-            -(nh0 as f64 + nl)
+            let val = parse_number!(u64::from_le_bytes([0, 0, 0, 0, 0, 0, *h0, *l]));
+            (val as f64) * -0.1
         }
         [h1, h0, b'.', l] => {
-            let nh1 = parse_number!(h1) * 10;
-            let nh0 = parse_number!(h0);
-            let nl = parse_number!(l) as f64 * 0.1;
-
-            (nh1 + nh0) as f64 + nl
+            let val = parse_number!(u64::from_le_bytes([0, 0, 0, 0, 0, *h1, *h0, *l]));
+            (val as f64) * 0.1
         }
         [h0, b'.', l] => {
-            let nh0 = parse_number!(h0);
-            let nl = parse_number!(l) as f64 * 0.1;
-
-            nh0 as f64 + nl
+            let val = parse_number!(u64::from_le_bytes([0, 0, 0, 0, 0, 0, *h0, *l]));
+            (val as f64) * 0.1
         }
         _ => unreachable!(),
     }
@@ -261,33 +281,24 @@ fn get_key(input: &[u8]) -> HashKey {
     match len {
         0 => 0,
         1..=15 => {
-            let offset_bit = (16 - len) * 8;
-            let a = unsafe { ptr.read() };
-            (a.to_be() >> offset_bit) << offset_bit
+            let mask = u128::MAX >> ((16 - len) * 8);
+            let a = unsafe { ptr.read() } & mask;
+            a.to_be()
         }
         _ => unsafe { ptr.read() }.to_be(),
     }
 }
 
-fn main() -> Result<(), BrcErrors> {
-    let worker_count = available_parallelism()?;
-    let fd = if let Some(file_path) = args().nth(1) {
-        let fd = std::fs::File::open(file_path)?;
-        Ok(fd)
-    } else {
-        Err(BrcErrors::InvalidPath)
-    }?;
+#[inline]
+fn process_file(mmap: Mmap, chunk_size: usize) -> Result<(), BrcErrors> {
+    let mmap_arc = Arc::new(mmap);
 
-    let mmap = Mmap::new(fd)?;
-
-    let task_results = std::thread::scope(|s| {
-        let chunk_size: usize = mmap.len() / worker_count;
-        let mmap_bytes = mmap.as_byte_slice();
-        let mut handles: Vec<ScopedJoinHandle<StationHashMap>> =
-            Vec::with_capacity(worker_count.into());
-
-        for chunk in ChunkedLinesIter::from(mmap_bytes).chunk_by(chunk_size) {
-            let handle = s.spawn(|| {
+    let job_handles: Vec<JoinHandle<StationHashMap>> = ChunkedLinesIter::from(mmap_arc)
+        .chunk_by(chunk_size)
+        .into_iter()
+        .map(|slice| {
+            thread::spawn(move || {
+                let chunk = slice.as_byte_slice();
                 let mut store: StationHashMap =
                     StationHashMap::with_capacity_and_hasher(512, City64HasherBuilder::default());
 
@@ -312,7 +323,9 @@ fn main() -> Result<(), BrcErrors> {
                             [b';', ..] => line.len() - 6,
                             [_, b';', ..] => line.len() - 5,
                             [_, _, b';', ..] => line.len() - 4,
-                            _ => unreachable!(),
+                            _ => {
+                                unreachable!();
+                            }
                         };
 
                     let s_val = parse_f64(&line[(split_pos + 1)..line.len()]);
@@ -342,20 +355,13 @@ fn main() -> Result<(), BrcErrors> {
                 }
 
                 store
-            });
-
-            handles.push(handle);
-        }
-
-        handles
-            .into_iter()
-            .flat_map(|h| h.join())
-            .collect::<Vec<StationHashMap>>()
-    });
+            })
+        })
+        .collect();
 
     let mut merge_btree: BTreeMap<HashKey, StationData> = BTreeMap::new();
 
-    for result_block in task_results {
+    for result_block in job_handles.into_iter().flat_map(|handle| handle.join()) {
         for (key, value) in result_block {
             match merge_btree.get_mut(&key) {
                 Some(station) => {
@@ -368,55 +374,43 @@ fn main() -> Result<(), BrcErrors> {
         }
     }
 
-    let mut stdout_writer = BufWriter::new(std::io::stdout());
-    let last_idx = merge_btree.len() - 1;
-
-    _ = stdout_writer.write(b"{")?;
-
-    for (idx, (_, value)) in merge_btree.into_iter().enumerate() {
-        stdout_writer.write_fmt(format_args!(
-            "{}={:.1}/{:.1}/{:.1}",
-            unsafe { from_utf8_unchecked(&value.name) },
-            value.min,
-            value.total / value.count,
-            value.max,
-        ))?;
-
-        if idx == last_idx {
-            stdout_writer.write(b"}")?;
-        } else {
-            stdout_writer.write(b", ")?;
-        }
-    }
-
-    stdout_writer.flush()?;
-
+    // let mut stdout_writer = BufWriter::new(std::io::stdout());
+    // let last_idx = merge_btree.len() - 1;
+    //
+    // _ = stdout_writer.write(b"{")?;
+    //
+    // for (idx, (_, value)) in merge_btree.into_iter().enumerate() {
+    //     stdout_writer.write_fmt(format_args!(
+    //         "{}={:.1}/{:.1}/{:.1}",
+    //         unsafe { from_utf8_unchecked(&value.name) },
+    //         value.min,
+    //         value.total / value.count,
+    //         value.max,
+    //     ))?;
+    //
+    //     if idx == last_idx {
+    //         stdout_writer.write(b"}")?;
+    //     } else {
+    //         stdout_writer.write(b", ")?;
+    //     }
+    // }
+    //
+    // stdout_writer.flush()?;
     Ok(())
 }
 
-#[cfg(test)]
-mod test {
-    use crate::ChunkedLinesIter;
+fn main() -> Result<(), BrcErrors> {
+    let worker_count = available_parallelism()?;
+    let fd = if let Some(file_path) = args().nth(1) {
+        let fd = std::fs::File::open(file_path)?;
+        Ok(fd)
+    } else {
+        Err(BrcErrors::InvalidPath)
+    }?;
 
-    #[test]
-    fn chunk_test() {
-        let input: &[u8] = &[
-            5, 5, 5, 5, 5, b'\n', 1, b'\n', 2, 2, b'\n', 4, 4, 4, 4, b'\n', 1, b'\n', 1, b'\n', 3,
-            3, 3, b'\n',
-        ];
+    let mmap = Mmap::new(fd)?;
+    let chunk_size: usize = mmap.len() / worker_count;
+    process_file(mmap, chunk_size)?;
 
-        let mut iterator = ChunkedLinesIter::from(input).chunk_by(5);
-
-        let first = iterator.next().unwrap();
-        assert_eq!(first, &[5, 5, 5, 5, 5, b'\n']);
-
-        let second = iterator.next().unwrap();
-        assert_eq!(second, &[1, b'\n', 2, 2, b'\n']);
-
-        let third = iterator.next().unwrap();
-        assert_eq!(third, &[4, 4, 4, 4, b'\n']);
-
-        let forth = iterator.next().unwrap();
-        assert_eq!(forth, &[1, b'\n', 1, b'\n', 3, 3, 3, b'\n']);
-    }
+    Ok(())
 }
